ac4ff6dea18116566c99e4f521d074792da3910d
PedroBarbosa <psbpedrobarbosa@gmail.com>
2023-01-16 18:59:42 +0000

    Fix issues with running the notebook cells with papermill

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM docker.io/mambaorg/micromamba:0.27.0
 LABEL Name=paperIntronicBenchmark Version=1.0
-COPY --chown=$MAMBA_USER:$MAMBA_USER conda_env.yml /tmp/env.yaml
+
+COPY conda_env.yml /tmp/env.yaml
 RUN micromamba install -y -n base -f /tmp/env.yaml && \
     micromamba clean --all --yes
 WORKDIR /work
--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 mkdir -p out
-chmod -R 777 out
+chmod -R 777 out data
 
-docker run -v $PWD/data:/work/data -v $PWD/scripts:/work/scripts -v $PWD/out:/work/out --rm intronic_eval papermill -k python3 run_benchmark.ipynb /work/out/out.ipynb
-docker run -v $PWD/data:/work/data -v $PWD/scripts:/work/scripts -v $PWD/out:/work/out --rm intronic_eval jupyter nbconvert --to html /work/out/out.ipynb
+docker run -v $PWD/data:/work/data -v $PWD/scripts:/work/scripts -v $PWD/out:/work/out --rm --user=root intronic_eval papermill --log-output -k python3 intronic_benchmark.ipynb /work/out/out.ipynb
+docker run -v $PWD/data:/work/data -v $PWD/scripts:/work/scripts -v $PWD/out:/work/out --rm --user=root intronic_eval jupyter nbconvert --to html /work/out/out.ipynb
