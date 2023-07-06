815803165cd8eb88eee33a30481de1af2d00b13b
PedroBarbosa <psbpedrobarbosa@gmail.com>
2023-07-06 18:20:34 +0100

    Rename AP to auPRC

--- scripts/4_splicing_associated_results_comparison.R
+++ scripts/4_splicing_associated_results_comparison.R
@@ -28,11 +28,11 @@ df <- map2_df(list_files,
                 mutate(analysis = .y) %>% 
                 select(c('tool', 'norm_mcc', 'weighted_norm_mcc', 'weighted_F1', 'total_p', 'total_n',
                          'tp', 'fp', 'tn', 'fn',
-                         'fraction_nan', 'weighted_accuracy', 'auROC', 'average_precision', 'analysis'))) 
+                         'fraction_nan', 'weighted_accuracy', 'auROC', 'auPRC', 'analysis'))) 
 
 # Remove AP score from tools with more than 50% of missing data since ROC curves are not drawn in such cases
-df$average_precision <- ifelse(df$tool == "MLCsplice" & df$analysis == "branchpoint", NA, df$average_precision)
-df$average_precision <- ifelse(df$tool == "IntSplice2" & df$analysis == "branchpoint", NA, df$average_precision)
+df$auPRC <- ifelse(df$tool == "MLCsplice" & df$analysis == "branchpoint", NA, df$auPRC)
+df$auPRC <- ifelse(df$tool == "IntSplice2" & df$analysis == "branchpoint", NA, df$auPRC)
 
 to_heatmap <- df
 df <- df %>% drop_na()
@@ -72,7 +72,7 @@ df <- bind_rows(pe, partial_ir)
 ###########################
 ###### Per region #########
 ###########################
-ggplot(df %>% filter(analysis != "branchpoint"), aes(x=group, y=average_precision,fill=group)) +
+ggplot(df %>% filter(analysis != "branchpoint"), aes(x=group, y=auPRC,fill=group)) +
   geom_boxplot(alpha=0.5) + 
   scale_fill_manual(values=c("#619CFF", "#F8766D")) +
   geom_point() +
@@ -88,7 +88,7 @@ ggplot(df %>% filter(analysis != "branchpoint"), aes(x=group, y=average_precisio
         plot.title = element_text(size=16, hjust = 0.5),
         legend.text = element_text(size=13)) +
   ylim(0.2, 1) +
-  labs(x = '', y = 'Average Precision')
+  labs(x = '', y = 'auPRC')
 
 ########################
 ###### Per tool ########
@@ -146,19 +146,19 @@ df_ <- df_ %>% mutate(p.adj_lower_1 = ifelse(p.adj_lower_1 == "", "", sub("^", "
 df_$region <- factor(df_$region, levels = c("Acceptor associated", "Exonic-like", "New splice donor", "Donor downstream"))
 df_ <- df_ %>% filter(!is.na(p.value))
 
-ggplot(df_, aes(x=reorder(tool, -average_precision, median))) +
-  geom_boxplot(aes(x=group, y=average_precision, fill=group, alpha=0.5), outlier.shape=NA) + 
-  geom_point(aes(x=group, y=average_precision, fill = region), position = position_dodge(width=0.2), size=3, shape = 21)  +
+ggplot(df_, aes(x=reorder(tool, -auPRC, median))) +
+  geom_boxplot(aes(x=group, y=auPRC, fill=group, alpha=0.5), outlier.shape=NA) + 
+  geom_point(aes(x=group, y=auPRC, fill = region), position = position_dodge(width=0.2), size=3, shape = 21)  +
   scale_fill_manual(values=c("coral4", "aquamarine4", "darkslateblue", "darkgrey", "#619CFF", "#F8766D")) +
   geom_text(data = subset(df_, region == "Acceptor associated"), aes(x = 1.5, y = 1.21, label = p.adj, color=colors), size = 2.5) +
   geom_text(data = subset(df_, region == "Exonic-like"), aes(x = 1.5, y = 1.09, label = p.adj, color=colors), size = 2.5) +
   geom_text(data = subset(df_, region == "New splice donor"), aes(x = 1.5, y = 1.03, label = p.adj, color=colors), size = 2.5) +
   geom_text(data = subset(df_, region == "Donor downstream"), aes(x = 1.5, y = 1.15, label = p.adj, color=colors), size = 2.5) +
   scale_colour_manual(values = c("aquamarine4", "coral4", "darkgrey", "darkslateblue")) +
-  facet_wrap(reorder(tool, -average_precision, median) ~ ., nrow=2) +
+  facet_wrap(reorder(tool, -auPRC, median) ~ ., nrow=2) +
   theme_bw() +
   ylim(0.2, 1.21) +
-  ylab('Average precision') +
+  ylab('auPRC') +
   theme(legend.title=element_blank(),
         axis.text.y = element_text(size=13),
         axis.title.y = element_text(size=15),
@@ -187,7 +187,7 @@ partial_ir_all <- partial_ir %>%
 # "#A6CEE3", "#A6761D", "#D95F02", "#FDCDAC", "#A6D854", "#984EA3", "#D9D9D9",
 # "#CCEBC5", "#FFFFB3", "#E7298A", "#FFED6F", "#66A61E", "#666666", "#BEAED4", "#E6F5C9"
 
-pe_all$tool = factor(pe_all$tool, levels = df %>% filter(analysis == "pe_acceptor_associated") %>% arrange(-average_precision) %>% pull(tool))
+pe_all$tool = factor(pe_all$tool, levels = df %>% filter(analysis == "pe_acceptor_associated") %>% arrange(-auPRC) %>% pull(tool))
 labels_pe <- c(paste0("Acceptor associated\nN pos=", 
                       pe_all %>% filter(analysis == "pe_acceptor_associated") %>% pull(total_p) %>% first(), 
                       ";\nN neg=", 
@@ -236,15 +236,15 @@ pal <- c("#B15928", "#377EB8", "#E5D8BD", "#FBB4AE", "#E7298A", "#984EA3", "#7dc
 
 all <- bind_rows(pe_all, partial_ir_all)
 all <- left_join(all, label_map)
-all$tool <- factor(all$tool, levels = all %>% filter(analysis == "pe_acceptor_associated") %>% arrange(-average_precision) %>% pull(tool))
+all$tool <- factor(all$tool, levels = all %>% filter(analysis == "pe_acceptor_associated") %>% arrange(-auPRC) %>% pull(tool))
 all$label <- factor(all$label, levels = c(labels_pe, labels_partial_ir))
 all$group <- factor(all$group, levels=c('Pseudoexon activation','Partial intron retention'))
-ggplot(all, aes(x=label, y=average_precision, group=tool, color =tool)) +
+ggplot(all, aes(x=label, y=auPRC, group=tool, color =tool)) +
   geom_bump(smooth = 15, size = 0.5, alpha = 1) + 
   geom_point(size=5, position = position_dodge(0.15)) +
   scale_color_manual(values = pal) +
   facet_grid(~group, scales = "free_x") +
-  labs(x = '', y = "Average precision") +
+  labs(x = '', y = "auPRC") +
   #scale_x_discrete(labels = c(labels_pe, labels_partial_ir)) +
   theme_bw() +
   theme(legend.title=element_blank(),
@@ -274,12 +274,12 @@ unique_tools <- unique(to_heatmap$tool)
 
 fill_missing_analysis <- function(i){
   group_name <- group_names[[i]] 
-  df_ <- list_dfs[[i]] %>% select(tool,auROC, average_precision)
+  df_ <- list_dfs[[i]] %>% select(tool,auROC, auPRC)
   missing_tools <- setdiff(unique_tools, df_$tool)
   
   not_perfomed <- tibble(tool = missing_tools, 
                            auROC = rep(c(0.18), times = length(missing_tools)),
-                           average_precision = rep(c(0.18), times = length(missing_tools)))
+                           auPRC = rep(c(0.18), times = length(missing_tools)))
   df_ <- bind_rows(df_, not_perfomed)
   df_$analysis <- group_name
   return(df_)
@@ -292,19 +292,19 @@ list_dfs <- to_heatmap %>%
 group_names <- sort(unique(to_heatmap$analysis))
 out <- lapply(seq_along(list_dfs), fill_missing_analysis) %>% bind_rows() %>%
   mutate(auROC = replace_na(auROC, 0.19),
-         average_precision = replace_na(average_precision, 0.19))
+         auPRC = replace_na(auPRC, 0.19))
 
 # Change MLCsplice and dbscSNV. They were run, but had all predictions missed, b
 # eing wrongly assigned to the "no run for variant region" class
-# out <-out %>% mutate(average_precision=case_when(tool=="MLCsplice" & analysis == "pe_new_donor" ~ 0.19,
+# out <-out %>% mutate(auPRC=case_when(tool=="MLCsplice" & analysis == "pe_new_donor" ~ 0.19,
 #                                    tool=="dbscSNV" & analysis %in% c("pe_new_donor", "partial_ir_new_donor") ~ 0.19,
-#                                    TRUE ~ average_precision))
-out <-out %>% mutate(average_precision=case_when(tool=="IntSplice2" & analysis %in% c("partial_ir_acceptor_associated", "pe_acceptor_associated") ~ 0.19,
-                                                 TRUE ~ average_precision))
+#                                    TRUE ~ auPRC))
+out <-out %>% mutate(auPRC=case_when(tool=="IntSplice2" & analysis %in% c("partial_ir_acceptor_associated", "pe_acceptor_associated") ~ 0.19,
+                                                 TRUE ~ auPRC))
 
 out <-
-  out %>% mutate(average_precision_bins = cut(
-    average_precision,
+  out %>% mutate(auPRC_bins = cut(
+    auPRC,
     breaks = c(-2.1,-1.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1),
     labels = c(
       "Too many missing predictions",
@@ -322,8 +322,8 @@ out <-
     )
   ))
 
-final_heat <- out %>% select(-auROC, -average_precision) %>% pivot_wider(names_from = analysis, values_from = average_precision_bins) %>% column_to_rownames(var = 'tool')
-final_heat <- out %>% select(-auROC, -average_precision_bins) %>% pivot_wider(names_from = analysis, values_from = average_precision) %>% column_to_rownames(var = 'tool')
+final_heat <- out %>% select(-auROC, -auPRC) %>% pivot_wider(names_from = analysis, values_from = auPRC_bins) %>% column_to_rownames(var = 'tool')
+final_heat <- out %>% select(-auROC, -auPRC_bins) %>% pivot_wider(names_from = analysis, values_from = auPRC) %>% column_to_rownames(var = 'tool')
 tool_order <- c("SpliceAI", "Pangolin", "CI-SpliceAI", "ConSpliceML", "AbSplice-DNA", "PDIVAS", "SPiP", "SQUIRLS", "TraP", "MMSplice", "SPIDEX", 
                 "kipoiSplice4", "MLCsplice","LaBranchoR", "BPHunter", "BPP", "SVM_BP_finder", "IntSplice2", "MaxEntScan", "SpliceRover", "Spliceator", "DSSP", "HAL", "HEXplorer", "ESRseq", "ESEfinder")
 col_order <- c("branchpoint", "pe_acceptor_associated", "partial_ir_acceptor_associated", "pe_sre",
@@ -369,7 +369,7 @@ htmp <- Heatmap(
   cluster_row_slices = F,
   cluster_rows = F,
   rect_gp = gpar(col = "black", lwd = 1),
-  name = "Average precision",
+  name = "auPRC",
   border = T,
   col = col_fun,
   row_names_side = 'left',
